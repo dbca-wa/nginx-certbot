9c94f1f4075e495871315843b503c2f6567b5452
Adon Metcalfe <adonm@fastmail.fm>
2021-01-08 17:36:20 +0800

    host build steps

---
+++ build-host.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# run with an ssh key configured for root on the host to build
+# e.g. ./build-host.sh my-nginx-host.some.domain
+ssh root@$1 "curl -sfL https://get.k3s.io | sh -"
+rsync -Pvvar /var/nginx-etc/ root@$1:/var/nginx-etc/
+ssh root@$1 "k3s kubectl apply -f https://github.com/dbca-wa/nginx-certbot-logstash/blob/main/kubernetes-deployment.yaml"
