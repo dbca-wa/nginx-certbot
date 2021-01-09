a8dfae2a4738ad0ee0bba8a37bc4bb6587ba6750
DBCA ASI <asi@dpaw.wa.gov.au>
2021-01-09 12:08:35 +0800

    rsync verbose

--- reload-hosts.sh
+++ reload-hosts.sh
@@ -7,6 +7,6 @@ git commit -am 'testconfig autocommit' && git push
 popd
 for host in "$@"
 do
-    rsync -ar /var/nginx-etc/ root@$host:/var/nginx-etc/
+    rsync -avr /var/nginx-etc/ root@$host:/var/nginx-etc/
     ssh root@$host "k3s kubectl rollout restart deployment nginx"
 done
