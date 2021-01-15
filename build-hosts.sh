a284d3c51260774b9aa354b9816a57462d11b874
Ashley Felton <ashley.felton@dbca.wa.gov.au>
2021-01-15 13:23:15 +0800

    Add step to build-hosts to scale filebeat service down & up.

--- build-hosts.sh
+++ build-hosts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # run with an ssh key configured for root on the target hosts
-# e.g. ./build-host.sh nginx1.domain nginx2.domain
+# e.g. ./build-hosts.sh nginx1.domain nginx2.domain
 for host in "$@"
 do
     ssh root@$host "apt -y install docker.io"
@@ -9,5 +9,6 @@ do
     ssh root@$host "mkdir -p /var/run/nginx-sigsci"
     ssh root@$host "docker stack deploy nginx -c /var/nginx-etc/nginx-certbot-logstash/docker-compose.yaml"
     ssh root@$host "ln -svf /var/nginx-etc/nginx-certbot-logstash/reload-nginx.sh /usr/local/bin/reload-nginx"
+    ssh root@$host "docker service scale -d nginx_filebeat=0 && sleep 2 && docker service scale -d nginx_filebeat=1"
     ssh root@$host "cp -v /var/nginx-etc/nginx-crons /etc/cron.d/nginx-crons"
 done
