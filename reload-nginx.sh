b398e862f370b22cd71f52fdf9fe0a4cb2e278e5
Ashley Felton <ashley.felton@dbca.wa.gov.au>
2021-01-15 12:30:08 +0800

    Tweak reload-nginx.sh

--- reload-nginx.sh
+++ reload-nginx.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker exec $(docker ps | grep nginx_nginx | awk '{print $1}') bash -c "nginx -t && echo $hostname reloading... && nginx -s reload"
+docker exec $(docker ps --filter name=nginx_nginx --quiet) bash -c "nginx -t && echo $hostname reloading... && nginx -s reload"
