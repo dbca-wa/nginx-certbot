04af6d1d92ebefc02cadc425fa9af8f786b504d4
Ashley Felton <ashley.felton@dbca.wa.gov.au>
2021-01-22 09:33:42 +0800

    Update a couple of references of logstash > filebeat.

--- build-hosts.sh
+++ build-hosts.sh
@@ -7,8 +7,8 @@ do
     ssh root@$host "docker swarm init"
     rsync -avr --delete /var/nginx-etc/ root@$host:/var/nginx-etc/
     ssh root@$host "mkdir -p /var/run/nginx-sigsci"
-    ssh root@$host "docker stack deploy nginx -c /var/nginx-etc/nginx-certbot-logstash/docker-compose.yaml"
-    ssh root@$host "ln -svf /var/nginx-etc/nginx-certbot-logstash/reload-nginx.sh /usr/local/bin/reload-nginx"
+    ssh root@$host "docker stack deploy nginx -c /var/nginx-etc/nginx-certbot-filebeat/docker-compose.yaml"
+    ssh root@$host "ln -svf /var/nginx-etc/nginx-certbot-filebeat/reload-nginx.sh /usr/local/bin/reload-nginx"
     ssh root@$host "docker service scale -d nginx_filebeat=0 && sleep 2 && docker service scale -d nginx_filebeat=1"
     ssh root@$host "cp -v /var/nginx-etc/nginx-crons /etc/cron.d/nginx-crons"
 done
