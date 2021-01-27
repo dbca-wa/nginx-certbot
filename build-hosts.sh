f1ad885f49433d5d2041d4021761a935357e592e
DBCA ASI <asi@dpaw.wa.gov.au>
2021-01-27 13:00:15 +0800

    Ensure that /var/log/nginx exists on build targets.

--- build-hosts.sh
+++ build-hosts.sh
@@ -7,6 +7,7 @@ do
     ssh root@$host "docker swarm init"
     rsync -avr --delete /var/nginx-etc/ root@$host:/var/nginx-etc/
     ssh root@$host "mkdir -p /var/run/nginx-sigsci"
+    ssh root@$host "mkdir -p /var/log/nginx"
     ssh root@$host "docker stack deploy nginx -c /var/nginx-etc/nginx-certbot-filebeat/docker-compose.yaml"
     ssh root@$host "ln -svf /var/nginx-etc/nginx-certbot-filebeat/reload-nginx.sh /usr/local/bin/reload-nginx"
     ssh root@$host "docker service scale -d nginx_filebeat=0 && sleep 2 && docker service scale -d nginx_filebeat=1"
