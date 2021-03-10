b01af521c0b7d9c4623a6ccaaeaeb6e84395db3d
DBCA ASI <asi@dpaw.wa.gov.au>
2021-03-10 17:00:55 +0800

    avoid deleting certs

--- build-hosts.sh
+++ build-hosts.sh
@@ -5,7 +5,7 @@ for host in "$@"
 do
     ssh root@$host "curl -fsSL https://get.docker.com | sh"
     ssh root@$host "docker swarm init"
-    rsync -avr --delete /var/nginx-etc/ root@$host:/var/nginx-etc/
+    rsync -avur /var/nginx-etc/ root@$host:/var/nginx-etc/
     ssh root@$host "mkdir -p /var/log/nginx"
     ssh root@$host "docker stack deploy nginx -c /var/nginx-etc/nginx-certbot/docker-compose.yaml"
     ssh root@$host "ln -svf /var/nginx-etc/nginx-certbot/reload-nginx.sh /usr/local/bin/reload-nginx"
